a6857cd4b120b001ea9ccd974743cb4d7a0a0845
Sushant-Stha <sshre733@mtroyal.ca>
2020-02-24 17:13:45 -0700

    Updates

---
+++ precache-manifest.d73e593d79f65f992620c4ad1eae198d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "662ade4fb3efa03cd98564c2f7c1061c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "267f792ccaabcd8affb2",
+    "url": "/static/css/main.6541834f.chunk.css"
+  },
+  {
+    "revision": "4f9afb9720a9fa496b09",
+    "url": "/static/js/2.f6bb1d67.chunk.js"
+  },
+  {
+    "revision": "e13163c277047ad8a8c284f199a3fd56",
+    "url": "/static/js/2.f6bb1d67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "267f792ccaabcd8affb2",
+    "url": "/static/js/main.b3775c22.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
